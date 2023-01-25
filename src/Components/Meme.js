213b9ac3a0f036a7f05443ad31c6e0fd14b2725c
Bailee <bailee.roby@gmail.com>
2023-01-24 22:33:29 -0800

    updating meme js and creating memes data

--- src/Components/Meme.js
+++ src/Components/Meme.js
@@ -1,12 +1,22 @@
+import memesData from '../memesData.js'
 
 export default function Meme() {
+
+    function getMemeImage() {
+        const memesArray = memesData.data.memes
+        const randomNumber = Math.floor(Math.random () * memesArray.length)
+
+       const url = memesArray[randomNumber].url
+       console.log(url)
+    }
+
     return (
         <div className = "meme">
             <div className = "meme--div">
                 <input type = "text" placeholder = "Top Text" className = "meme--input"></input>
                 <input type = "text" placeholder = "Bottom Text" className = "meme--input"></input>
             </div>
-                <button className = "meme--button">Get a new meme image 🖼️</button>
+                <button onClick = {getMemeImage} className = "meme--button">Get a new meme image 🖼️</button>
         </div>
     )
 }
